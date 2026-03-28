182b18df85d359ab6c3be8e75b72a30b6a1aa148
Andy <support@skillkraftz.com>
2026-03-28 18:09:24 -0400

    Working?

--- apps/portal/next.config.ts
+++ apps/portal/next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.join(__dirname, "../../"),
+  outputFileTracingExcludes: {
+    "*": [
+      "./node_modules/@swc/**",
+      "./node_modules/@next/swc-linux-x64-gnu/**",
+      "./node_modules/playwright-core/**",
+      "./node_modules/@playwright/**",
+      "./node_modules/@img/**",
+      "./node_modules/sharp/**",
+    ],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
